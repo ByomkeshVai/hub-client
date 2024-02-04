e9310741fd185b84c53c36511a834974abee7cba
byomkesh2580 <habibthemerchant@gmail.com>
2024-02-04 12:52:48 +0600

    createSemester

--- src/components/form/PHSelect.tsx
+++ src/components/form/PHSelect.tsx
@@ -1,24 +1,23 @@
 import { Form, Select } from "antd";
+import { Controller } from "react-hook-form";
 
 type PHSelectProps = {
   label: string;
+  name: string;
+  options: { value: string; label: string; disabled?: boolean }[];
 };
 
-const PHSelect = ({ label }: PHSelectProps) => {
-  const handleChange = (value: string) => {
-    console.log(`selected ${value}`);
-  };
+const PHSelect = ({ label, name, options }: PHSelectProps) => {
   return (
     <Form.Item label={label}>
-      <Select
-        defaultValue="lucy"
-        style={{ width: "100%" }}
-        onChange={handleChange}
-        options={[
-          { value: "jack", label: "Jack" },
-          { value: "lucy", label: "Lucy" },
-          { value: "Yiminghe", label: "yiminghe" },
-        ]}
+      <Controller
+        rules={{
+          required: true,
+        }}
+        name={name}
+        render={({ field }) => (
+          <Select {...field} style={{ width: "100%" }} options={options} />
+        )}
       />
     </Form.Item>
   );
--- src/pages/admin/academicManagement/academicSemester/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/academicSemester/CreateAcademicSemester.tsx
@@ -1,19 +1,42 @@
 import { FieldValues, SubmitErrorHandler } from "react-hook-form";
 import PHForm from "../../../../components/form/PHForm";
-import PHInput from "../../../../components/form/PHInput";
-import { Col, Flex } from "antd";
+import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../../components/form/PHSelect";
+import { semesterOptions } from "../../../../constant/semester";
+import { monthOptions } from "../../../../constant/global";
+
+const getCurrentYear = new Date().getFullYear();
+const yearOptions = [0, 1, 2, 3, 4].map((value) => ({
+  value: String(getCurrentYear + value),
+  label: String(getCurrentYear + value),
+}));
 
 const CreateAcademicSemester = () => {
   const onsubmit: SubmitErrorHandler<FieldValues> = (data) => {
-    console.log(data);
+    const name = semesterOptions[Number(data.name) - 1].label;
+    const semesterData = {
+      name,
+      code: data.name,
+      year: data.year,
+      startMonth: data.startMonth,
+      endMonth: data.endMonth,
+    };
+    console.log(semesterData);
   };
+
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onsubmit}>
-          <PHInput type="text" name="text" label="name" />
-          <PHSelect label="name" />
+          <PHSelect label="name" name="name" options={semesterOptions} />
+          <PHSelect label="year" name="year" options={yearOptions} />
+          <PHSelect
+            label="Start Month"
+            name="startMonth"
+            options={monthOptions}
+          />
+          <PHSelect label="End Month" name="endMonth" options={monthOptions} />
+          <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
     </Flex>
